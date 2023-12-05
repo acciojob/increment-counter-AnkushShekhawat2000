d53b5b2a17ca41a74d5a3067687b619be90658c0
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 09:45:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+ // Get the counter element and the increment button
+    const counterElement = document.getElementById("counter");
+    const incrementBtn = document.getElementById("incrementBtn");
+
+    // Initial counter value
+    let counterValue = 0;
+
+    // Function to handle button click
+    function handleIncrement() {
+      // Display the current counter value in an alert
+      alert("Un-incremented value: " + counterValue);
+      
+      // Increment the counter value
+      counterValue++;
+
+      // Update the counter element text
+      counterElement.textContent = counterValue;
+    }
